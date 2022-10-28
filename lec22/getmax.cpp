80c95ea6c727dbdbb7b4fb2a1cf8a4471c65acd3
ricky-aufvaa <aufvaa@gmail.com>
2022-10-28 16:04:11 +0530

    dsa

--- lec22/getmax.cpp
+++ lec22/getmax.cpp
@@ -3,11 +3,22 @@
 #include <iostream>
 using namespace std;
 
-void getmax(string s) {
+char getmax(string s) {
   int arr[26] = {0};
-  cout << s[4] - 'a' << endl;
-  cout << s[5] - 'a' << endl;
-  cout << s[6] - 'a' << endl;
+  int number = 0;
+  for (int i = 0; i < s.length(); i++) {
+    number = s[i] - 'a';
+    arr[number]++;
+  }
+  int maxi = -1, ans = 0;
+  for (int i = 0; i < 26; i++) {
+    if (maxi < arr[i]) {
+      ans = i;
+      maxi = arr[i];
+    }
+  }
+  char finalans = 'a' + maxi;
+  return finalans;
 }
 
 int getlength(char a[]) {
@@ -23,5 +34,5 @@ int main() {
   cin >> a;
   int length = getlength(a);
   cout << length << endl;
-  getmax(a);
+  cout << getmax(a);
 }
